2e007d8a1457cb1d713937c3d15e878a93b35253
Adrian Pabe <adrianpabs14@gmail.com>
2021-02-03 19:31:37 +0800

    Log conditionals

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,26 @@ const deletePerson = (event, id) => {
 	console.log(event);
 	people = people.filter((person) => person.id !== id);
 }
+
+let num = 5;
+
 </script>
+
+{#if num > 20}
+	<p>Greater than 20</p>
+{:else if num > 5}
+	<p>Greater than 5</p>
+{:else}
+	<p>Less than 5</p>
+{/if}
 
 <main>
 	{#each people as person (person.id)}
 		<div>
 			<h4>{person.name}</h4>
+			{#if person.armColor === 'black'}
+				<p><strong>Master Android!</strong></p>
+			{/if}
 			<p>{person.age} years old, {person.armColor} arm</p>
 			<button on:click={(e) => deletePerson(e, person.id)}>delete</button>
 		</div>
